9363cda717b66f370c6b607e86bdf6c389a75a30
zoehud <147034977+zoehud@users.noreply.github.com>
2023-12-20 19:32:00 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
 const cards = document.querySelectorAll('.memory-card');
+
 let hasFlippedCard = false;
+let lockBoard = false;
+let flippedCard;
+
 function flipCard() {
-  // Toggle the 'flip' class to show the back of the card
-  this.classList.toggle('flip');
+  if (lockBoard) return;
+
+  this.classList.add('flip');
+
+  if (!hasFlippedCard) {
+    hasFlippedCard = true;
+    flippedCard = this;
+  } else {
+    // You can perform any logic related to the flipped card here
+    // For example, you can access the flippedCard.dataset.framework
+    console.log('Flipped Card:', flippedCard.dataset.framework);
+  }
 }
 
-// Add a click event listener to each card
+function resetBoard() {
+  [hasFlippedCard, lockBoard] = [false, false];
+  flippedCard = null;
+}
+
+(function shuffle() {
+  cards.forEach(card => {
+    let randomPos = Math.floor(Math.random() * 16);
+    card.style.order = randomPos;
+  });
+})();
+
 cards.forEach(card => card.addEventListener('click', flipCard));
