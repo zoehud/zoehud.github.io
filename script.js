8c8a5c2186359f1ddcea8f355870c8170396d7c8
juliette-salles <72603693+juliette-salles@users.noreply.github.com>
2023-12-20 17:10:37 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+const cards = document.querySelectorAll('.memory-card');
+
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
+
+function flipCard() {
+  if (lockBoard) return;
+  if (this === firstCard) return;
+
+  this.classList.add('flip');
+
+  if (!hasFlippedCard) {
+    hasFlippedCard = true;
+    firstCard = this;
+
+    return;
+  }
+
+  secondCard = this;
+  checkForMatch();
+}
